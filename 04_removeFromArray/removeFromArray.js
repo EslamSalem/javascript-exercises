8530e59f23cea9a428ab0cf9986da3b78e15d8f2
EslamSalem <eslamsalem2.es@gmail.com>
2024-11-01 15:58:37 +0200

    upload solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr = [], ...args) {
+  for (let i = 0; i < args.length; i++) {
+    index = arr.indexOf(args[i]);
+    while (index !== -1) {
+      arr.splice(index, 1);
+      index = arr.indexOf(args[i]);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
