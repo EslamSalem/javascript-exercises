ac8066953abc39d0f191d74c71c9ad824af221e1
EslamSalem <eslamsalem2.es@gmail.com>
2024-11-01 15:25:08 +0200

    upload solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+  let chars = str.split('');
+  let reversed = "";
 
+  for (let i = chars.length - 1; i >= 0; i--) {
+    reversed += chars[i];
+  }
+
+  return reversed;
 };
 
 // Do not edit below this line
