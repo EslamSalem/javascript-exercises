4bf652afa14a01a79dcdd6e54b151264cf645669
EslamSalem <eslamsalem2.es@gmail.com>
2024-12-31 18:24:49 +0200

    08 solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers = []) {
+	return numbers.reduce((total, number) => total + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers = []) {
+  return numbers.reduce((total, number) => total * number, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let fact = 1;
+  for (n; n >= 1; n--) {
+    fact *= n;
+  }
+  return fact;
 };
 
 // Do not edit below this line
