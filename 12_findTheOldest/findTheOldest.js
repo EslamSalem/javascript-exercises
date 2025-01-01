99131da1d8182cfdea38338d36bcee1990058f0f
EslamSalem <eslamsalem2.es@gmail.com>
2025-01-01 20:40:36 +0200

    12 solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const oldest = people.reduce(
+    (obj, person) => {
+      let age = 0;
+      if (!person.yearOfDeath) {
+        age = new Date().getFullYear() - person.yearOfBirth;
+      } else {
+        age = person.yearOfDeath - person.yearOfBirth;
+      }
 
+      if (age > obj.age) {
+        obj.name = person.name;
+        obj.age = age;
+      }
+      return obj;
+    },
+    { name: "", age: 0 }
+  );
+
+  return oldest;
 };
 
 // Do not edit below this line
