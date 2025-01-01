a52a4fda19e206bbd42b134907eadfe1297e0755
EslamSalem <eslamsalem2.es@gmail.com>
2025-01-01 17:54:31 +0200

    10-fibonacci solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) return "OOPS";
+  const fib =
+    (1 / Math.sqrt(5)) * ((1 + Math.sqrt(5)) / 2) ** n -
+    (1 / Math.sqrt(5)) * ((1 - Math.sqrt(5)) / 2) ** n;
 
+    return Math.round(fib);
 };
 
 // Do not edit below this line
