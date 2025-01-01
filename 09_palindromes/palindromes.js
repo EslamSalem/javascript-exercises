12022878ad666ac4fe25e72f618c392324a50f7e
EslamSalem <eslamsalem2.es@gmail.com>
2025-01-01 17:19:48 +0200

    09-palindromes solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const chars = str.toLowerCase().match(/\w/g);
 
+  for (let i = 0; i < chars.length / 2; i++) {
+    if (chars[i] !== chars[chars.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 // Do not edit below this line
